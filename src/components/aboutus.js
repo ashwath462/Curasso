c7877d09c1d6006473009c52b10a8a6b6380c283
unknown <ashwatharora462@gmail,com>
2022-04-07 16:31:09 +0530

    card css updated

--- src/components/aboutus.js
+++ src/components/aboutus.js
@@ -14,28 +14,34 @@ function AboutUs() {
           <Container fluid >
             <FadeIn delay={2000} transitionDuration={1000}>
             <Row>
-            <Col className="text-center-abt" md="4">
+            <Col className = "text-center-abt" md="4">
+              <div className="card">
                 <h2 className="title revealOnScroll">Mission</h2>
-                <h5 className="description" style={{color:"#34495e",fontWeight:400}}>
+                <p>
                 To provide innovative, creative, and effective integrated brand marketing & public relations solutions which help our clients grow their businesses and realize true Creative and Marketing goals.
-                </h5>
+                </p>
+              </div>
             </Col>
 
-            <Col className="text-center-abt" md="4">
+            <Col className = "text-center-abt" md="4">
+            <div className="card">
                 <h2 className="title revealOnScroll">Vision</h2>
-                <h5 className="description" style={{color:"#34495e",fontWeight:400}}>          
+                <p>         
                 Delivering result-oriented branding and marketing programs as well as public relations campaigns that enhance our clients’ awareness, improve their sales and foster their growth.
-                </h5>
+                </p>
+              </div>
             </Col>
             </Row>
             </FadeIn>
             <FadeIn delay={2000} transitionDuration={1000}>
             <Row>
-            <Col className="text-center text-center-abt" md="4">
+            <Col className = "text-center-abt" md="6">
+            <div className="card">
                 <h2 className="title revealOnScroll">About Us</h2>
-                <h5 className="description" style={{color:"#34495e",fontWeight:400}} >
+                <p>
                 Minimal Holic™ is a creative solutions provider for all kinds of businesses. With a sole mission to help your brand stand out from the crowd, our experienced team will provide you with everything from branding, web content, graphics, app UI/UX, copywriting, creative content to social media management and digital marketing.
-                </h5>
+                </p>
+              </div>
             </Col>
             </Row>
             </FadeIn>
